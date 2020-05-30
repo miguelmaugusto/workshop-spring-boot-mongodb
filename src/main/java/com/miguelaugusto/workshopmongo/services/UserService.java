5cbf6a6764b1c576b903fedd206cde3be1f1e526
Miguel Augusto <miguelmaugusto@gmail.com>
2020-05-30 18:11:51 +0100

    Inserção de user com POST

--- src/main/java/com/miguelaugusto/workshopmongo/services/UserService.java
+++ src/main/java/com/miguelaugusto/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.miguelaugusto.workshopmongo.domain.User;
+import com.miguelaugusto.workshopmongo.dto.UserDTO;
 import com.miguelaugusto.workshopmongo.repository.UserRepository;
 
 @Service
@@ -23,5 +24,13 @@ public class UserService {
 		
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Não foi encontrado nenhum usuário com o id " + id));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
